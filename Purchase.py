df9454eaaeb63c8e3fc10a57afcc56d3589b4674
tikitakaaps <50847222+callernameannouncer@users.noreply.github.com>
2020-03-30 15:38:43 +0300

    Purchase class constructor with default values

--- Purchase.py
+++ Purchase.py
@@ -1,2 +1,6 @@
 class Purchase:
-    pass
+    def __init__(self, internet_connection, phones_lines, cell_phones, price):
+        self.internet_connection = True
+        self.phones_lines = 0
+        self.cell_phones = []
+        self.price = 0
