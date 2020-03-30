1c3664b8b4e778db1be72ade0a37e62cb2d9fa3e
tikitakaaps <50847222+callernameannouncer@users.noreply.github.com>
2020-03-30 15:43:11 +0300

    Removed the unnecessary parameters for Purchase

--- Purchase.py
+++ Purchase.py
@@ -1,5 +1,5 @@
 class Purchase:
-    def __init__(self, internet_connection, phones_lines, cell_phones, price):
+    def __init__(self):
         self.internet_connection = True
         self.phones_lines = 0
         self.cell_phones = []
