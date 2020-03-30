ccd65a56d05a20c3ea5637a46df709e3d6970a24
MapeSVK <michal.moravik@icloud.com>
2020-03-30 11:46:20 +0200

    Purchase class created

--- Purchase.py
+++ Purchase.py
@@ -1,6 +1,6 @@
 class Purchase:
-  def __init__(internet_connection, phone_lines, cell_phones, price):
-    self.internet_connection = internet_connection
-    self.phone_lines = phone_lines
-    self.cell_phones = cell_phones
-    self.price = price
+    def __init__(internet_connection, phone_lines, cell_phones, price):
+        self.internet_connection = internet_connection
+        self.phone_lines = phone_lines
+        self.cell_phones = cell_phones
+        self.price = price
