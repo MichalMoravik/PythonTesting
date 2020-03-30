31c2171bd01fd2c4acca9ccaa1ca8a2d242241ca
Abdulmuaz Alshaikhli <abdu0445@edu.easj.dk>
2020-03-30 22:01:08 +0200

    Add the "internet_connection" method and the "buy" method
    Missing comments will add them later.

--- Purchase.py
+++ Purchase.py
@@ -4,3 +4,22 @@ class Purchase:
         self.phones_lines = 0
         self.cell_phones = []
         self.price = 0
+
+
+        def internet_connection(self, user_choices):
+            if user_choices:
+                self.price += 200
+            else:
+                self.price -=200
+
+            return self.price
+
+
+        
+        def buy(self):
+            if self.price == 0:
+                try:
+                    print()
+                
+                except ValueError:
+                    print("Please add something to the list")
