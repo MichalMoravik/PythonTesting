32d15158d0ceb1f7958d80280b099f5dfb2b99ec
MapeSVK <michal.moravik@icloud.com>
2020-04-01 11:18:13 +0200

    update to select and unselect phone methods

--- purchase.py
+++ purchase.py
@@ -1,11 +1,11 @@
 # example of possible phones dictionary
-phonesDict = {
-    "Motorola G99": 800,
-    "iPhone 99": 6000,
-    "Samsung Galaxy 99": 1000,
-    "Sony Xperia 99": 900,
-    "Huawei 99": 900,
-}
+# phonesDict = {
+#     "Motorola G99": 800,
+#     "iPhone 99": 6000,
+#     "Samsung Galaxy 99": 1000,
+#     "Sony Xperia 99": 900,
+#     "Huawei 99": 900,
+# }
 
 
 class Purchase:
